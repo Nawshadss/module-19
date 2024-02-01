4675b13c9dab0df65ad88c207f14b4cc0569489e
Nawshadss <hossainmdnawshad@gmail.com>
2024-02-01 19:21:33 +0600

    done

---
+++ vowelCheck.js
@@ -0,0 +1,15 @@
+let stig = "Check whether a string contains all the vowels";
+
+if (stig.includes("a")) {
+  console.log(true);
+} else if (stig.includes("e")) {
+  console.log(true);
+} else if (stig.includes("i")) {
+  console.log(true);
+} else if (stig.includes("o")) {
+  console.log(true);
+} else if (stig.includes("u")) {
+  console.log(true);
+} else {
+  console.log(false);
+}
